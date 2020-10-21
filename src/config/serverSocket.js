0ea72692dc2470b92e4eadaf9c4d5cbbf3c141c4
Bogdan Shevchuk <11015532+SeNeKa97@users.noreply.github.com>
2020-10-22 00:11:38 +0300

    Update serverSocket.js

--- src/config/serverSocket.js
+++ src/config/serverSocket.js
@@ -2,4 +2,4 @@ export const host = "smart-ranch-backend.herokuapp.com";
 export const port = "9000";
 
 //export const address = "http://" + host + ":" + port;
-export const address = "http://" + host;
+export const address = "https://smart-ranch-backend.herokuapp.com";
